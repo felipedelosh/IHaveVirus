31d960bff120180ec695713b29c9d3bfa25db046
felipedelosh <doctorfhernandez@hotmail.com>
2024-08-19 00:22:35 -0500

    ping

--- attack.py
+++ attack.py
@@ -14,7 +14,7 @@ Esto es para tratar de defenderme
 URL attacks me:
 
 https://n0pkf36.findpubdue.live/lqkcgvut/?u=63fkp0n&o=uh7pmz8&f=1&sid=t1~t0edj5cp4iejludsvza1vobl&fp=Md%2Bb%2FogcsL6hTN%2FIdl07EQ%3D%3D
-https://n0pkf36.findpubdue.live/vnjpyomd/?u=63fkp0n&o=uh7pmz8&f=1&sid=t2~saav2u21tvhgu0fe4vblc3jh&fp=PgJxGtpSbeXystT1Izv8Cg%3D%3D
+https://n0pkf36.findpubdue.live/vnjpymod/?u=63fkp0n&o=uh7pmz8&f=1&sid=t2~saav2u21tvhgu0fe4vblc3jh&fp=PgJxGtpSbeXystT1Izv8Cg%3D%3D
 """
 
 options = Options()
@@ -28,10 +28,6 @@ _ip = "http://89.208.107.49/"
 _base_url = "https://n0pkf36.findpubdue.live/"
 _url= f"{_base_url}vnjpyomd"
 
-# Abre la página
-driver.get(_url)
-
-
 def calculateNewUser(qty_letters_usser):
     """
     Return [a-z]*qty
@@ -40,10 +36,19 @@ def calculateNewUser(qty_letters_usser):
     return letters
 
 
+def getURL():
+    return f"{_base_url}{calculateNewUser(8)}"
+
+
 
 # Recarga la página varias veces
-for _ in range(1000):  # Cambia el valor si quieres recargar más o menos veces
-    driver.refresh()
+for _ in range(1000):
+    try:
+        driver.get(getURL())
+        print(f"get:{getURL()}")
+    except:
+        print(f"error:{getURL()}")
+
     time.sleep(1)  # Espera 1 segundo entre recargas para no sobrecargar
 
 # Cierra el navegador
